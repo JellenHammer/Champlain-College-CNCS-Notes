ed9b9fb90024604154753ff90c6f3816529b7a2c
unknown <clad8895@proton.me>
2024-09-29 11:34:10 -0400

    Adding dot notation file

---
+++ Linux Automation and Scripting (SYS-320-03)/October 1/CallingFunctionsDOTNOTATION.ps1
@@ -0,0 +1,8 @@
+﻿. (Join-Path $PSScriptRoot Apache Logs.ps1)
+. (Join-Path $PSScriptRoot "Apache Logs (parsing)".ps1)
+
+clear
+$ErrorActionPreference = "SilentlyContinue"
+
+Apache-Logs -page index.html -statusCode 200 -browser chrome
+Apache-Logs-Full -page poop.html -statusCode 404 -browser chrome
