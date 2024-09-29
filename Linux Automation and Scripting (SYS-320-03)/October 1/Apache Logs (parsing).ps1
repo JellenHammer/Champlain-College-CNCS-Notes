d39ee2fcf2021ab93b26ddb6e4104579feb8c4ce
unknown <clad8895@proton.me>
2024-09-29 11:27:27 -0400

    Adding new powershell script for the last Oct 1 Assignment

---
+++ Linux Automation and Scripting (SYS-320-03)/October 1/Apache Logs (parsing).ps1
@@ -0,0 +1,24 @@
+﻿function Apache-Logs-Full {
+param (
+
+[string] $page,
+[string] $statusCode,
+[string] $browser
+)
+
+$logsUnformatted = Get-Content C:\xampp\apache\logs\*.log | Select-String -Pattern $page | Select-String -Pattern $statusCode | Select-String -Pattern $browser
+$logsSplit = $logsUnformatted -split " "
+
+$tableFull = @()
+
+$tableFull += [PSCustomObject]@{
+        "IP" = $logsSplit | Select-Object -index 0
+        "Date/Time" = $logsSplit | Select-Object -index 3
+        "Method" = $logsSplit | Select-Object -index 4
+        "Request" = $logsSplit | Select-Object -index 5,6,7
+        "Status Code" = $logsSplit | Select-Object -index 8
+        "Browser" = $logsSplit | Select-Object -index 21
+        "Referrer" = $logsSplit | Select-Object -index 10
+}
+$tableFull
+}
