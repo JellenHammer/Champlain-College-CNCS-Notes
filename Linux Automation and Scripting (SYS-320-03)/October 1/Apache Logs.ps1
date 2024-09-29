834a8a64a2f46fb1fc06eed5c8f8311344c860e8
unknown <clad8895@proton.me>
2024-09-29 11:09:29 -0400

    Adding October 1st Files

---
+++ Linux Automation and Scripting (SYS-320-03)/October 1/Apache Logs.ps1
@@ -0,0 +1,32 @@
+﻿function Apache-Logs {
+param (
+
+[string] $page,
+[string] $statusCode,
+[string] $browser
+)
+
+$notfounds = Get-Content C:\xampp\apache\logs\*.log | Select-String -Pattern $page | Select-String -Pattern $statusCode | Select-String -Pattern $browser
+$notfounds2 = $notfounds -split " "
+$tableFull = @()
+$tableFull += [PSCustomObject]@{
+        "IP" = $notfounds2 | Select-Object -index 0
+        "Date" = $notfounds2 | Select-Object -index 3
+        "Request" = $notfounds2 | Select-Object -index 5,6,7
+        "Status Code" = $notfounds2 | Select-Object -index 8
+        "Browser" = $notfounds2 | Select-Object -index 21
+}
+$tableFull
+
+$regex = [regex] "\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+$IP_ADDR = $notfounds2 | Select-Object -index 0
+$ipsUnorganized = $regex.Matches($IP_ADDR)
+
+$ips  = @()
+for($i=0; $i -lt $ipsUnorganized.Count; $i++){
+    $ips += [PSCustomObject]@{ "IP" = $ipsUnorganized -replace '[0-9]'.Value; }
+    }
+    $ipsoften = $ips | Where-Object { $_.IP -ilike "10.*" }
+    $counts = $ipsoften | measure
+    $counts | Select-Object Count
+}
---
+++ Linux Automation and Scripting (SYS-320-03)/October 1/IP Address Regex #1.ps1
@@ -0,0 +1,12 @@
+﻿$notfounds = Get-Content C:\xampp\apache\logs\*.log | Select-String -Pattern ' 404'
+
+$regex = [regex] "\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+$ipsUnorganized = $regex.Matches($notfounds)
+
+$ips  = @()
+for($i=0; $i -lt $ipsUnorganized.Count; $i++){
+    $ips += [PSCustomObject]@{ "IP" = $ipsUnorganized -replace '[0-9]'.Value; }
+    }
+    $ipsoften = $ips | Where-Object { $_.IP -ilike "10.*" }
+    $counts = $ipsoften | measure
+    $counts | Select-Object Count, Name
