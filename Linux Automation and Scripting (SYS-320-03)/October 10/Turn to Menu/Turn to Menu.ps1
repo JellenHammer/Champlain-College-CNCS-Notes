053855a1502a4b9de9e9a201638532a423e8622f
unknown <clad8895@proton.me>
2024-10-28 12:18:05 -0400

    Adding turn to menu & more

--- Linux Automation and Scripting (SYS-320-03)/October 10/Turn to Menu/Turn to Menu.ps1
+++ Linux Automation and Scripting (SYS-320-03)/October 10/Turn to Menu/Turn to Menu.ps1
@@ -1,6 +1,7 @@
 ﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
 . (Join-Path $PSScriptRoot Users.ps1)
 . (Join-Path $PSScriptRoot Event-Logs.ps1)
+. (Join-Path $PSScriptRoot Event-Logs-Full.ps1)
 . (Join-Path $PSScriptRoot String-Helper.ps1)
 
 clear
@@ -21,20 +22,41 @@ while($operation){
     Write-Host $Prompt | Out-String
     $choice = Read-Host 
    
-
     if($choice -eq 1){
-        Write-Host "Please enter your html page"
-        $promptHTML = Read-Host
-        Write-Host "Please enter your status code"
-        $promptSTATUS = Read-Host
-        Write-Host "Please enter your browser"
-        $Browser = Read-Host
-        Write-Host | Apache-Logs -page $promptHTML -statusCode $promptSTATUS -browser $Browser
+
+        $LogsUnformat = Get-Content 'C:\Users\champuser\CNCS-Tech-Portfolio\Linux Automation and Scripting (SYS-320-03)\October 17\access.log'
+        $tableFull = @()
+
+        for($i=0; $i -lt $LogsUnformat.Count; $i++){
+
+        $line = $LogsUnformat[$i] -split " "
+
+        <#$IP =  $lines[$i] -split " " | Select-Object -index 0
+        $TIME =  $lines[$i] -split " " | Select-Object -index 3
+        $METHOD =  $lines[$i] -split " " | Select-Object -index 4
+        $REQUEST =  $lines[$i] -split " " | Select-Object -index 5,6,7
+        $STATUS =  $lines[$i] -split " " | Select-Object -index 8
+        $BROWSER =  $lines[$i] -split " " | Select-Object -index 18
+        $REFERRER =  $lines[$i] -split " " | Select-Object -index 10#>
+        
+
+        $tableFull += [PSCustomObject]@{
+                "IP" =  $line | Select-Object -index 0
+                "Date/Time" =  $line | Select-Object -index 3
+                "Method" =  $line | Select-Object -index 4
+                "Request" =  $line | Select-Object -index 5, 6, 7
+                "Status Code" =  $line | Select-Object -index 8
+                "Browser" =  $line | Select-Object -index 18
+                "Referrer" =  $line | Select-Object -index 10
         }
+        }
+        
+return $tableFull | Format-Table
+}
         
 
     elseif($choice -eq 2){
-
+    # Need administrator access when running
         $name = Read-Host -Prompt "Please enter the username for the user's failed login logs"
         $chkUser = checkUser $name
             if($chkUser -ne $false){
