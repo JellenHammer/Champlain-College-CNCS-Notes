d4e24999ba5c74add8b54084cbce4d31fd07e74a
JellenHammer <clad8895@proton.me>
2025-02-18 15:43:29 -0500

    Adding secure-ssh script

---
+++ System Administration & Network Services II (SYS-265-02)/Linux/centos7/secure-ssh.sh
@@ -0,0 +1,7 @@
+read -p "Enter username:" username
+sudo useradd -m -d /home/$username -s /bin/bash $username
+sudo mkdir /home/$username/.ssh
+sudo cp '/home/connorj/Champlain-College-CNCS-Notes/System Administration & Network Services II (SYS-265-02)/Linux/Public-Keys/id_rsa.pub' /home/$username/.ssh/authorized_keys
+sudo chmod 700 /home/$username/.ssh
+sudo chmod 600 /home/$username/.ssh/authorized_keys
+sudo chown -R $username:$username /home/$username/.ssh
