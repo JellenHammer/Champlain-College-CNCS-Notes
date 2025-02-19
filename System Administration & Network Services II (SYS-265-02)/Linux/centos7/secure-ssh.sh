656cbac07d4bdecada52dea14e31ba532e186454
JellenHammer <clad8895@proton.me>
2025-02-19 14:48:10 +0000

    Finishing up secure ssh script

--- System Administration & Network Services II (SYS-265-02)/Linux/centos7/secure-ssh.sh
+++ System Administration & Network Services II (SYS-265-02)/Linux/centos7/secure-ssh.sh
@@ -1,4 +1,6 @@
-read -p "Enter username:" username
+#!/usr/bin/bash
+username=$1
+#read -p "Enter username:" username
 sudo useradd -m -d /home/$username -s /bin/bash $username
 sudo mkdir /home/$username/.ssh
 sudo cp '/home/connorj/Champlain-College-CNCS-Notes/System Administration & Network Services II (SYS-265-02)/Linux/Public-Keys/id_rsa.pub' /home/$username/.ssh/authorized_keys
